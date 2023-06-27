6698ff9a87a56d304c5a36300ae1801df3c7e834
ddamme05 <ddamme05@gmail.com>
2023-06-27 15:05:23 -0400

    pass state down to tasklist

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -45,113 +45,14 @@ function TaskList() {
     }
   }
 
-  function removeTask(taskId) {
-    const updatedTasks = tasks.filter((task) => task.id !== taskId);
-    setTasks(updatedTasks);
-  }
-
-  function handleEditClick(taskId) {
-    const task = tasks.find((task) => task.id === taskId);
-    setEditTaskId(taskId);
-    setEditTaskTitle(task.title);
-  }
-
-  function handleSaveClick(taskId) {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === taskId) {
-        return {
-          ...task,
-          title: editTaskTitle,
-        };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-    setEditTaskId(null);
-    setEditTaskTitle("");
-  }
-
-  function handleCompleteClick(taskId) {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === taskId) {
-        return {
-          ...task,
-          //Change false to true
-          completed: !task.completed,
-        };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  }
-
-  function toggleMoreInfo(taskId) {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === taskId) {
-        return {
-          ...task,
-          //Change false to true, triggers box enlargement
-          showMoreInfo: !task.showMoreInfo,
-        };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  }
-
   return (
     <div className="container">
       <h1>Task List</h1>
       <div className="task-list-container">
-        <ul>
-          {tasks.map((task) => {
-            if (editTaskId === task.id) {
-              return (
-                <li key={task.id}>
-                  <div>
-                    <input
-                      type="text"
-                      value={editTaskTitle}
-                      onChange={(event) => setEditTaskTitle(event.target.value)}
-                    />
-                    <button onClick={() => handleSaveClick(task.id)}>
-                      Save
-                    </button>
-                  </div>
-                </li>
-              );
-            } else {
-              return (
-                <li key={task.id}>
-                  <div>
-                    <strong>Title:</strong> {task.title}{" "}
-                    {task.completed ? "(Completed)" : ""}
-                    <br />
-                    {task.showMoreInfo ? (
-                      <div>
-                        <strong>Due Date:</strong> {task.dueDate} <br />
-                        <strong>Created Date:</strong> {task.createdDate} <br />
-                      </div>
-                    ) : null}
-                    <button onClick={() => handleEditClick(task.id)}>
-                      Edit
-                    </button>
-                    <button onClick={() => handleCompleteClick(task.id)}>
-                      {/* If completed true, mark complete, else mark incomplete. */}
-                      {task.completed ? "Mark Incomplete" : "Mark Complete"}
-                    </button>
-                    <button onClick={() => removeTask(task.id)}>
-                      Remove Task
-                    </button>
-                    <button onClick={() => toggleMoreInfo(task.id)}>
-                      {task.showMoreInfo ? "Less Info" : "More Info"}
-                    </button>
-                  </div>
-                </li>
-              );
-            }
-          })}
-        </ul>
+       <TaskList  taskTitle={taskTitle} editTaskId={editTaskId} editTaskTitle={editTaskTitle} setTasks={setTasks} 
+       setEditTaskId={setEditTaskId}
+       setEditTaskTitle={setEditTaskTitle}
+       />
         <form onSubmit={handleTaskSubmit}>
           <input
             type="text"
